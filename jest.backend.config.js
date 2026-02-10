535fd3a030ae44b6334187d30d9d491282b59ede
sheenkerr <46549752+sheenkerr@users.noreply.github.com>
2026-02-10 19:17:28 +0800

    Update JEST  config settings

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,20 +1,24 @@
 export default {
-  // display name
-  displayName: "backend",
+	// display name
+	displayName: "backend",
 
-  // when testing backend
-  testEnvironment: "node",
+	// when testing backend
+	testEnvironment: "node",
 
-  // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+	// which test to run
+	testMatch: [
+		"<rootDir>/controllers/*.test.js",
+		"<rootDir>/helpers/*.test.js",
+		"<rootDir>/middlewares/*.test.js",
+	],
 
-  // jest code coverage
-  collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
-  coverageThreshold: {
-    global: {
-      lines: 100,
-      functions: 100,
-    },
-  },
+	// jest code coverage
+	collectCoverage: true,
+	collectCoverageFrom: ["controllers/**", "helpers/**", "middlewares/**"],
+	coverageThreshold: {
+		global: {
+			lines: 100,
+			functions: 100,
+		},
+	},
 };
