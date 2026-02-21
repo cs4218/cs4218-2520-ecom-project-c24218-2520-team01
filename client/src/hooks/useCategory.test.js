3cc130023f487421ed6d2f9b8c842fea82f75427
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2026-02-21 10:53:36 +0800

    Add unit tests for useCategory hook

--- client/src/hooks/useCategory.test.js
+++ client/src/hooks/useCategory.test.js
@@ -1,3 +1,4 @@
+import { afterEach, beforeEach, describe, expect, jest } from "@jest/globals";
 import { renderHook, waitFor } from "@testing-library/react";
 import axios from "axios";
 import useCategory from "./useCategory";
@@ -5,62 +6,95 @@ import useCategory from "./useCategory";
 // Mock axios
 jest.mock("axios");
 
-describe("Unit test for useCategory hook", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+describe("Hook for fetching categories", () => {
+    describe("Unit tests for useCategory hook", () => {
+        // Set up variables for our test cases
+        let consoleSpy;
 
-    test("Return an initially empty array", async () => {
-        // Arrange
-        axios.get.mockResolvedValueOnce({ data: { category: [] } });
-
-        // Act
-        const { result } = renderHook(() => useCategory());
-
-        // Assert
-        // We need to await for the state to finish updaating before finishing the test
-        await waitFor(() => {
-            expect(result.current).toEqual([]);
-        });
-    });
-
-    test("Fetch and return categories successfully", async () => {
-        // Arrange
-        const mockCategories = [
-            { id: 1, name: "Electronics" },
-            { id: 2, name: "Books" },
-        ];
-
-        axios.get.mockResolvedValueOnce({
-            data: { category: mockCategories },
+        // Before each test case we reset our variables / mocks
+        beforeEach(() => {
+            // Spy instead of mock because we might want to log in between tests.
+            consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => { });
+            jest.clearAllMocks();
         });
 
-        // Act
-        const { result } = renderHook(() => useCategory());
-
-        // Assert
-        await waitFor(() => {
-            expect(result.current).toEqual(mockCategories);
+        afterEach(() => {
+            consoleSpy.mockRestore();
         });
-        expect(axios.get).toHaveBeenCalledWith("/api/v1/category/get-category");
-    });
 
-    test("Handle errors that occue during the function call", async () => {
-        // Arrange
-        const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => { });
-        const mockError = new Error("Some error");
-        axios.get.mockRejectedValueOnce(mockError);
+        describe("Ensure initial state of the hook is configured correctly", () => {
+            test("Check initial state for categories is an empty list", () => {
+                // Act
+                const { result } = renderHook(() => useCategory());
 
-        // Act
-        const { result } = renderHook(() => useCategory());
+                // Assert
+                expect(result.current).toEqual([]);
+            });
+        })
 
-        // Assert
-        await waitFor(() => {
-            expect(consoleSpy).toHaveBeenCalledWith(mockError);
+        describe("Successfully retrieve categories from API & set it as a state", () => {
+            test("Fetch categories from API and set it as a state", async () => {
+                // Arrange
+                const mockCategories = [
+                    { id: 1, name: "Electronics", slug: "electronics" },
+                    { id: 2, name: "Books", slug: "books" },
+                ];
+                const API_URL = "/api/v1/category/get-category";
+
+                axios.get.mockResolvedValueOnce({
+                    data: { category: mockCategories },
+                });
+
+                // Act
+                const { result } = renderHook(() => useCategory());
+
+                // Assert
+                await waitFor(() => {
+                    expect(result.current).toEqual(mockCategories);
+                });
+                expect(axios.get).toHaveBeenCalledWith(API_URL);
+            });
+
+            test("Receieve empty categories list from API and set it as a state", async () => {
+                /**
+                 * Assumption: We should not have any issues even with an empty categories list
+                 * from the API call.
+                 */
+                // Arrange
+                const mockCategories = [];
+                const API_URL = "/api/v1/category/get-category";
+
+                axios.get.mockResolvedValueOnce({
+                    data: { category: mockCategories },
+                });
+
+                // Act
+                const { result } = renderHook(() => useCategory());
+
+                // Assert
+                await waitFor(() => {
+                    expect(result.current).toEqual([]);
+                });
+                expect(axios.get).toHaveBeenCalledWith(API_URL);
+            });
         });
-        expect(result.current).toEqual([]); // There should not be any change to our state
 
-        // Clean up the spy
-        consoleSpy.mockRestore();
+        describe("Errors reguarding axios", () => {
+            test("Axios throws an error during the execution of the function", async () => {
+                // Arrange
+                const mockError = new Error("Axios error");
+                axios.get.mockRejectedValueOnce(mockError);
+
+                // Act
+                const { result } = renderHook(() => useCategory());
+
+                // Assert
+                await waitFor(() => {
+                    expect(consoleSpy).toHaveBeenCalledWith(mockError);
+                });
+                // There should not be any changes in our state
+                expect(result.current).toEqual([]);
+            });
+        });
     });
-})
+});
