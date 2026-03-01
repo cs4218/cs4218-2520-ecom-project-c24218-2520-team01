29a1c12f2dbcad14bb6aae4ce5592adcd6caa22b
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2026-03-01 14:34:52 +0800

    Fix typos in orderModels.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -59,7 +59,7 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       default: "Not Process",
-      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+      enum: ["Not Process", "Processing", "Shipped", "Delivered", "Cancel"],
     },
   },
   { timestamps: true }
