eefabd3018fd8e49277827373cc5082a8c2beccc
ITLimJiaWei <jiaweilim77@gmail.com>
2026-03-22 15:24:28 +0800

    Modify config for ui tests

---
+++ playwright.config.cjs
@@ -0,0 +1,31 @@
+// @ts-check
+const { defineConfig, devices } = require("@playwright/test");
+
+module.exports = defineConfig({
+    testDir: "./tests/e2e",
+    globalSetup: "./tests/e2e/globalSetup.js",
+    globalTeardown: "./tests/e2e/globalTeardown.js",
+    fullyParallel: false,
+    retries: 0,
+    workers: 1,
+    reporter: "list",
+    timeout: 30000,
+    use: {
+        baseURL: "http://localhost:3000",
+        trace: "on-first-retry",
+    },
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        }
+    ],
+});
